f95ba21a2993f781cab1059ede577d1f6393aff6
Daniel <gilliland.daniel@gmail.com>
2013-04-11 17:10:44 -0700

    Adding old logic

--- Lab2/src/bioUtils/DNAUtil.java
+++ Lab2/src/bioUtils/DNAUtil.java
@@ -1,9 +1,31 @@
 package bioUtils;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class DNAUtil {
 	
 	public static String analyze(String dnaFile, int startIndex, int stopIndex, int winShift, int winSize) {
-		
+				
+		File file = new File(dnaFile);
+		Scanner fileScan;
+		try {
+			fileScan = new Scanner(file);
+
+			// Skip first line - does not contain sequence
+			fileScan.nextLine();
+
+			while (fileScan.hasNextLine()) {
+//				sequence += fileScan.nextLine();
+			}
+
+			fileScan.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 		
 		return "Test";
 	}
