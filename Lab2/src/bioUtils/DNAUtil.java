4706aabb62ba54101f75209f07bfe2fd69804cd9
Daniel <gilliland.daniel@gmail.com>
2013-04-16 17:22:23 -0700

    Ugly code. Attempt at single thread.

--- Lab2/src/bioUtils/DNAUtil.java
+++ Lab2/src/bioUtils/DNAUtil.java
@@ -1,6 +1,8 @@
 package bioUtils;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.text.DecimalFormat;
 import java.util.Map;
 import java.util.TreeMap;
@@ -10,23 +12,132 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 public class DNAUtil {
-	
-	
-	public static String singleAnalyze(String dnaFile, int startIndex, int stopIndex, int winShift, int winSize) {
-		
-		Map<Integer, Result> results = new ConcurrentHashMap<Integer, Result>();
+
+	public static String singleAnalyze(String dnaFile, int startIndex,
+			int stopIndex, int winShift, int winSize) {
+
+		Map<Integer, Result> results = new TreeMap<Integer, Result>();
 		File file = new File(dnaFile);
-		
+
 		// Check and initialize all variables
 		int curIndex = startIndex < 0 ? 1 : startIndex;
 		int lastIndex = stopIndex < 0 ? -1 : stopIndex;
 		int size = winSize < 0 ? 1000 : winSize;
 		int shift = winShift < 0 ? size : winShift;
-		
-		
-		return null;
+
+		BufferedReader reader = null;
+		try {
+			FileReader input = new FileReader(file);
+			reader = new BufferedReader(input);
+
+			// Skip the first line
+			reader.readLine();
+			// Get to the start index
+			for (int i = 1; i < curIndex;) {
+				int res = reader.read();
+				if (res > 40) {
+					i++;
+				}
+				if (res == -1) {
+					input.close();
+					return "";
+				}
+			}
+
+			boolean end = false;
+			boolean first = true;
+			int runningIndex = curIndex;
+
+			// While we still have characters to read
+			while (!end) {
+
+				// reset the reader
+				if (!first) {
+					try {
+						reader.reset();
+					} catch (Exception e) {
+						// Don't care - ignore this
+					}
+
+					// seek - not on the first iteration
+					for (int i = 0; i < shift;) {
+						int res = reader.read();
+						if (res > 40) {
+							i++;
+						}
+						if (res == -1) {
+							end = true;
+							break;
+						}
+					}
+
+					first = false;
+				}
+
+				// mark
+				StringBuilder sequence = new StringBuilder();
+				reader.mark(size + 1);
+
+				// read the next winsize sequence
+				for (int i = 0; i < size;) {
+					int res = reader.read();
+					if (res > 40) {
+						sequence.append((char) res);
+						i++;
+					}
+					if (res == -1) {
+						end = true;
+						break;
+					}
+				}
+
+				Result result = DNAUtil.findGcContentForSegment(sequence
+						.toString());
+				results.put(runningIndex, result);
+
+				runningIndex += winSize;
+
+				if (curIndex == lastIndex) {
+					end = true;
+				}
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		DecimalFormat df = new DecimalFormat("#.##");
+
+		// And build the result string
+		String rtn = "";
+
+		if (winShift > 0 || winSize > 0) {
+			rtn = "Range" + " ,\t" + "min" + " ,\t" + "max\n";
+
+			// Sort the results for better display
+			Map<Integer, Result> sortedRes = new TreeMap<Integer, Result>(
+					results);
+
+			for (Integer a : sortedRes.keySet()) {
+				rtn += a + " to " + (a + size - 1) + " ,\t"
+						+ df.format(results.get(a).min) + " ,\t"
+						+ df.format(results.get(a).max) + "\n";
+			}
+		} else {
+			double minTot = 0;
+			double maxTot = 0;
+
+			for (Integer a : results.keySet()) {
+				minTot += results.get(a).min;
+				maxTot += results.get(a).max;
+			}
+			rtn = "Total min ,\tTotal max\n";
+			rtn += df.format(minTot / results.keySet().size()) + " ,\t"
+					+ df.format(maxTot / results.keySet().size());
+		}
+
+		return rtn;
 	}
-	
 
 	public static String analyze(String dnaFile, int startIndex, int stopIndex,
 			int winShift, int winSize) throws Exception {
@@ -44,41 +155,47 @@ public class DNAUtil {
 		int lastIndex = stopIndex < 0 ? -1 : stopIndex;
 		int size = winSize < 0 ? 1000 : winSize;
 		int shift = winShift < 0 ? size : winShift;
-		
-		//Kick off the threads
-		for(int i = 0; i < size/shift; i++) {
-			service.execute(new CalcThread(file, curIndex, lastIndex, size, results));
+
+		// Kick off the threads
+		for (int i = 0; i < size / shift; i++) {
+			service.execute(new CalcThread(file, curIndex, lastIndex, size,
+					results));
 			curIndex += shift;
 		}
 
-		//Wait until all threads terminate (max wait of 1 minute - may need to make this longer)
+		// Wait until all threads terminate (max wait of 1 minute - may need to
+		// make this longer)
 		service.shutdown();
 		service.awaitTermination(1, TimeUnit.MINUTES);
-		
+
 		DecimalFormat df = new DecimalFormat("#.##");
-		
-		//And build the result string
+
+		// And build the result string
 		String rtn = "";
-		
-		if(winShift > 0 || winSize > 0) {
+
+		if (winShift > 0 || winSize > 0) {
 			rtn = "Range" + " ,\t" + "min" + " ,\t" + "max\n";
-			
-			//Sort the results for better display
-			Map<Integer, Result> sortedRes = new TreeMap<Integer, Result>(results);
-			
-			for(Integer a : sortedRes.keySet()) {
-				rtn += a + " - " + (a+size-1) + " ,\t" + df.format(results.get(a).min) + " ,\t" + df.format(results.get(a).max) + "\n";
+
+			// Sort the results for better display
+			Map<Integer, Result> sortedRes = new TreeMap<Integer, Result>(
+					results);
+
+			for (Integer a : sortedRes.keySet()) {
+				rtn += a + " to " + (a + size - 1) + " ,\t"
+						+ df.format(results.get(a).min) + " ,\t"
+						+ df.format(results.get(a).max) + "\n";
 			}
 		} else {
 			double minTot = 0;
 			double maxTot = 0;
-			
-			for(Integer a : results.keySet()) {
+
+			for (Integer a : results.keySet()) {
 				minTot += results.get(a).min;
 				maxTot += results.get(a).max;
 			}
 			rtn = "Total min ,\tTotal max\n";
-			rtn += df.format(minTot/results.keySet().size()) + " ,\t" + df.format(maxTot/results.keySet().size());
+			rtn += df.format(minTot / results.keySet().size()) + " ,\t"
+					+ df.format(maxTot / results.keySet().size());
 		}
 
 		return rtn;
