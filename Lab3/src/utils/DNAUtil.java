5dcee1a80e1ea6bc8091d765d1a3860f193ca696
Jake Muir <jimuir@calpoly.edu>
2013-04-22 20:47:55 -0700

    Finished most of the gene content and gene density stuff.  All that's left is 1e and 2b.

--- Lab3/src/utils/DNAUtil.java
+++ Lab3/src/utils/DNAUtil.java
@@ -12,6 +12,13 @@ import model.Gene;
 
 public class DNAUtil {
 	
+	public double avgCDSSpan = 0;
+	public Map<String, Double> averageCDSSizes = new HashMap<String, Double>();
+	public Map<String, Double> averageExonSizes = new HashMap<String, Double>();
+	public Map<String, Double> averageIntronSizes = new HashMap<String, Double>();
+	public int totalNucleotides = 0;
+	public int numGenes = 0;
+	
 	//Static methods again works for me
 
 	public static String calculateResults(String filepath) throws Exception {
@@ -20,7 +27,6 @@ public class DNAUtil {
 		Map<String, List<Gene>> genes = new HashMap<String, List<Gene>>();
 		
 		readFile(filepath, genes);
-		
 		
 		return displayInfo(genes);
 	}
@@ -92,6 +98,79 @@ public class DNAUtil {
 		
 		//All done
 		reader.close();
+	}
+	
+	
+	private void calculateGeneContentAndDensity(Map<String, List<Gene>> genes)
+	{
+		List<Gene> gList;
+		int mRNATotalSize = 0;
+		int mRNACount = 0;
+		Map<String, AttributeInfo> isoforms = new HashMap<String, AttributeInfo>();
+		Map<String, AttributeInfo> geneInfos = new HashMap<String, AttributeInfo>();
+		int totalCDS = 0;
+		
+		for(String name : genes.keySet()) {
+			 gList = genes.get(name);
+			 for(Gene g : gList)
+			 {
+				 //Average CDS Span
+				 if(g.getFeature().equals("mRNA"))
+				 {
+					 mRNACount++;
+					 mRNATotalSize += g.getStop() - g.getStart();
+				 }
+				 else if(g.getFeature().equals("CDS"))
+				 {
+					 totalCDS++;
+					//Average CDS size for each isoform
+					 String transcriptID = g.getAttributes().get("transcript_id");
+					 if(isoforms.containsKey(transcriptID))
+					 {
+						 isoforms.get(transcriptID).add(g.getStop() - g.getStart());
+					 }
+					 else
+					 {
+						 isoforms.put(transcriptID, new AttributeInfo(transcriptID, g.getStop() - g.getStart()));
+					 }
+					 
+					 //Average Exon Size for each gene that is a CDS
+					 String geneID = g.getAttributes().get("gene_id");
+					 if(geneInfos.containsKey(geneID))
+					 {
+						 geneInfos.get(geneID).add(g.getStop() - g.getStart());
+					 }
+					 else
+					 {
+						 geneInfos.put(geneID, new AttributeInfo(geneID, g.getStop() - g.getStart()));
+					 }
+				 }
+				 
+				 //Finding highest Nucleotide number
+				 this.totalNucleotides = (g.getStop() > this.totalNucleotides) ? g.getStop() : this.totalNucleotides;
+			 }
+		}
+		
+		//Average CDS Span
+		this.avgCDSSpan = (double)mRNATotalSize/mRNACount;
+		//Average CDS size for each isoform
+		for(String name : isoforms.keySet())
+		{
+			AttributeInfo iso = isoforms.get(name);
+			//Is it divided by number of distinct isoforms for all genes or just total number of isoforms???s
+			double avgCDS = (double)iso.totalSize/isoforms.keySet().size();
+			this.averageCDSSizes.put(iso.id, avgCDS);
+			//Average Intron Size for each isoform - not sure if this is right???
+			this.averageIntronSizes.put(iso.id,  (avgCDS - this.avgCDSSpan)/totalCDS);
+		}
+		//Average Exon Size for each gene
+		for(String name : geneInfos.keySet())
+		{
+			AttributeInfo geneInfo = geneInfos.get(name);
+			this.averageExonSizes.put(geneInfo.id, (double)geneInfo.totalSize/geneInfo.count);
+		}
+		//Number of genes
+		this.numGenes = geneInfos.keySet().size();
 	}
 	
 }
