7f9aa4837950fdd9e50754850bdc010dc20c89f1
Daniel <gilliland.daniel@gmail.com>
2013-06-02 16:49:47 -0700

    FASTA writing is almost complete for lab 6

--- Lab6/src/test/AllTests.java
+++ Lab6/src/test/AllTests.java
@@ -9,13 +9,33 @@ public class AllTests {
 
 	@Test
 	public void testStringCompare() {
-		String s1 = "AATGCTAGT";
-		String s2 = "GCTAGTCAT";
+		StringBuilder s1 = new StringBuilder("AATGCTAGT");
+		StringBuilder s2 = new StringBuilder("GCTAGTCAT");
 		
 		for(int i = 0; i < s1.length(); i++) {
 			if(DNAConcat.compareSubstrings(i, s1, s2)) {
 				Assert.assertEquals(3, i);
 			}
+		}
+	}
+	
+	@Test
+	public void testStringMerge() {
+		StringBuilder s1 = new StringBuilder("AATGCTAGT");
+		StringBuilder s2 = new StringBuilder("GCTAGTCAT");
+		
+		int overlap = DNAConcat.mergeStrings(s1, s2);
+		
+		Assert.assertEquals(6, overlap);
+		Assert.assertEquals("AATGCTAGTCAT", s1.toString());
+	}
+	
+	@Test
+	public void testApplication() {
+		try {
+			DNAConcat.processFiles("C:\\Users\\Daniel\\Documents\\D_erecta_2nd_3L_control_fasta1.zip", "C:\\Users\\Daniel\\Documents\\D_erecta_2nd_3L_control_gff1.zip");
+		} catch(Exception e) {
+			e.printStackTrace();
 		}
 	}
 	
--- Lab6/src/utils/DNAConcat.java
+++ Lab6/src/utils/DNAConcat.java
@@ -1,6 +1,11 @@
 package utils;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 
 import net.lingala.zip4j.core.ZipFile;
 
@@ -9,6 +14,9 @@ public class DNAConcat {
 	private static String FASTA_DIR = "~tempFASTA";
 	private static String GFF_DIR = "~tempGFF";
 	
+	private static String SUPER_CONTIG = "supercontig";
+	private static String SUPER_GFF = "supergff";
+	
 	/**
 	 * Does the main processing of the files
 	 * 
@@ -16,14 +24,54 @@ public class DNAConcat {
 	 * @param gffZip
 	 * @return
 	 */
-	public static String processFiles(String fastaZip, String gffZip) {
+	public static String processFiles(String fastaZip, String gffZip) throws Exception {
 		
 		File fastaDir = createTempDir(FASTA_DIR, fastaZip);
 		File gffDir = createTempDir(GFF_DIR, gffZip);
 		
+		File resultDir = new File("results");
+		resultDir.mkdir();
 		
+		File supercontig;
+		File superGFF;
 		
+		int supernumber = 1;
 		
+		String currentFASTA = "";
+		
+		File fastaFiles[] = fastaDir.listFiles();
+		
+		sortFiles(fastaFiles);
+		
+		if(fastaFiles.length > 0) {
+			currentFASTA = DNAFileReader.readFASTA(fastaFiles[0].getAbsolutePath());
+		}
+		
+		for(int i = 1; i < fastaFiles.length - 1; i++) {
+			File f = fastaFiles[i];
+			String toAppend = DNAFileReader.readFASTA(f.getAbsolutePath());
+			
+			int overlap = mergeStrings(new StringBuilder(currentFASTA), new StringBuilder(toAppend));
+			
+			//TODO play with the GFF's and fix them up
+			
+			if(overlap < 0) {
+				//Write the supercontig
+				File fastaToWrite = new File("results/" + SUPER_CONTIG + "" + supernumber + ".fasta");
+				fastaToWrite.createNewFile();
+				FileWriter fw = new FileWriter(fastaToWrite.getAbsoluteFile());
+				BufferedWriter writer = new BufferedWriter(fw);
+
+				writer.write(currentFASTA);
+				
+				//Write the superGFF
+				
+				writer.close();
+				supernumber++;
+				currentFASTA = toAppend;
+			}
+			
+		}
 		
 		deleteTempDir(fastaDir);
 		deleteTempDir(gffDir);
@@ -32,20 +80,74 @@ public class DNAConcat {
 	}
 	
 	/**
+	 * Sorts files in an array based on trailing number so
+	 * we read the files in order
+	 */
+	public static void sortFiles(File list[]) {
+		Arrays.sort(list, new Comparator<File>() {
+			@Override
+			public int compare(File o1, File o2) {
+				
+				int fileNum1 = findFileNum(o1.getName());
+				int fileNum2 = findFileNum(o2.getName());
+				
+				return fileNum1 - fileNum2;
+			}
+		});
+	}
+	
+	/**
+	 * returns the number of the file
+	 * used for sorting files 
+	 */
+	public static int findFileNum(String name) {
+		int dotIndex = name.indexOf(".");
+		int numIndex = dotIndex-1;
+		
+		boolean foundNum = true;
+		
+		while(foundNum) {
+			try {
+				numIndex--;
+				Integer.parseInt(name.charAt(numIndex) + "");
+			} catch(Exception e) {
+				foundNum = false;
+			}
+		}
+		
+		try {
+			return Integer.parseInt(name.substring(numIndex+1, dotIndex));
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		return 0;
+	}
+	
+	/**
 	 * Takes two FASTA strings and merges them into a single
-	 * FASTA string
+	 * FASTA string the first parameter is modified
 	 * 
 	 * @param one
 	 * @param two
-	 * @return
+	 * @return overlap value if there is overlap else -1
 	 */
-	private static String mergeStrings(String one, String two) {
+	public static int mergeStrings(StringBuilder one, StringBuilder two) {
 		
+		for(int i = 0; i < one.length(); i++) {
+			if(compareSubstrings(i, one, two)) {
+				int overlap = one.length() - i;				
+				
+				one.append(two.substring(overlap));
+				
+				return overlap;
+			}
+		}
 		
-		return null;
+		return -1;
 	}
 	
-	public static boolean compareSubstrings(int s1Index, String s1, String s2) {
+	public static boolean compareSubstrings(int s1Index, StringBuilder s1, StringBuilder s2) {
 		int i = s1Index;
 		int j = 0;
 		while(i < s1.length()) {
@@ -128,7 +230,6 @@ public class DNAConcat {
 		} else {
 			// if file, then delete it
 			file.delete();
-			System.out.println("File is deleted : " + file.getAbsolutePath());
 		}
 	}
 	
